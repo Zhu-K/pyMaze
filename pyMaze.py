5ed549da9cfe6f8163d498325cea27b6a5643c56
Kai Zhu <zhuk2@mcmaster.ca>
2021-02-18 00:44:12 -0500

    Customizable maze size

--- pyMaze.py
+++ pyMaze.py
@@ -5,33 +5,66 @@
 from time import time
 from libs.maze import Maze
 from tkinter import *
+from tkinter import messagebox
 import random
 
 GRAY = "#505050"
 DARKGRAY = "#303030"
+NODE_RAD = 5    # radii of the start, end nodes
+LINESPACE = 2   # gap between line and grid dots
+GRID_RAD = 2    # grid dot radius
+TOOLRG = 2500   # threshold for large maze
 
 master = Tk()
 master.configure(bg=DARKGRAY)
 master.title("Python Maze Solver")
 master.resizable(0,0)
 
-canvas_w = 500
-canvas_h = 500
-maze_w = 10
-maze_h = 10
-grid_width = canvas_w // maze_w
-grid_height = canvas_h // maze_h
+canvas_w = 600
+canvas_h = 600
+maze_w = 20
+maze_h = 20
+grid_width = canvas_w / maze_w
+grid_height = canvas_h / maze_h
 mz = Maze(maze_w, maze_h)
 
-def canvas_click(event):
-    x = event.x // grid_width
-    y = event.y // grid_height
-    
-    local_x = event.x - x * grid_width
-    local_y = event.y - y * grid_height
+def newMaze(*arg):
+    global maze_w, maze_h, grid_width, grid_height, mz, swidth, sheight
 
-    node = mz.nodes[y][x]
-    dir = -1
+    mz.start = None     # reset maze start/end
+    mz.end = None
+    c.delete("start")
+    c.delete("end")
+    
+    swidth, sheight = e_width.get(), e_height.get()
+
+    if not swidth.isnumeric() or int(swidth) < 2:
+        e_width.delete(0,END)
+        e_width.insert(0,str(maze_w))
+        swidth = maze_w
+    if not sheight.isnumeric() or int(sheight) < 2:
+        e_height.delete(0,END)
+        e_height.insert(0,str(maze_h))
+        sheight = maze_h
+    if int(swidth) * int(sheight) > TOOLRG:
+        if messagebox.askquestion("Caution", "The entered maze dimensions might be too large, continue regardless?") == "no":
+            return
+
+    maze_w = int(swidth)
+    maze_h = int(sheight)
+    grid_width = canvas_w / maze_w
+    grid_height = canvas_h / maze_h    
+    mz = Maze(maze_w, maze_h)
+    drawGrid()
+    drawMaze()
+
+def canvas_click(event):
+    x = int(event.x // grid_width)       # get grid coords
+    y = int(event.y // grid_height)
+    
+    local_x = event.x - x * grid_width      # get local coords inside grid
+    local_y = event.y - y * grid_height
+    dir = -1                    # initialize search direction
 
     if local_y < grid_height * 0.2:
         dir = 1 # top
@@ -52,12 +85,12 @@ def canvas_click(event):
             mz.end = None
         elif not mz.start:      # set start point 
             mz.start = (x, y)
-            c.create_oval((x+0.5)*grid_width-10, (y+0.5)*grid_height-10, (x+0.5)*grid_width+10, (y+0.5)*grid_height+10, fill="yellow", outline="yellow", tag = "start")  # resetting start
+            c.create_oval((x+0.5)*grid_width-NODE_RAD, (y+0.5)*grid_height- NODE_RAD, (x+0.5)*grid_width+NODE_RAD, (y+0.5)*grid_height+NODE_RAD, fill="yellow", outline="yellow", tag = "start")  # resetting start
         else:                   # set or move end point
             if mz.end:
                 c.delete("end")  # resetting start
             mz.end = (x, y)
-            c.create_oval((x+0.5)*grid_width-10, (y+0.5)*grid_height-10, (x+0.5)*grid_width+10, (y+0.5)*grid_height+10, fill="green", outline="green", tag = "end")  # resetting start
+            c.create_oval((x+0.5)*grid_width-NODE_RAD, (y+0.5)*grid_height-NODE_RAD, (x+0.5)*grid_width+NODE_RAD, (y+0.5)*grid_height+NODE_RAD, fill="green", outline="green", tag = "end")  # resetting start
 
 
     if dir > 0:
@@ -74,25 +107,25 @@ def drawWall(x, y, dir, unblocked):
 
     x0,y0,x1,y1 = 0,0,0,0
     if dir == 1:        # up
-        x0 = x * grid_width + 5
+        x0 = x * grid_width + LINESPACE
         y0 = y * grid_height
-        x1 = (x+1) * grid_width - 5
+        x1 = (x+1) * grid_width - LINESPACE
         y1 = y * grid_height
     elif dir == 2:      # right
         x0 = (x+1) * grid_width
-        y0 = y * grid_height + 5
+        y0 = y * grid_height + LINESPACE
         x1 = (x+1) * grid_width
-        y1 = (y+1) * grid_height - 5
+        y1 = (y+1) * grid_height - LINESPACE
     elif dir == 3:      # bottom
-        x0 = x * grid_width + 5
+        x0 = x * grid_width + LINESPACE
         y0 = (y+1) * grid_height
-        x1 = (x+1) * grid_width - 5
+        x1 = (x+1) * grid_width - LINESPACE
         y1 = (y+1) * grid_height
     else:               # left
         x0 = x * grid_width
-        y0 = y * grid_height + 5
+        y0 = y * grid_height + LINESPACE
         x1 = x * grid_width
-        y1 = (y+1) * grid_height - 5
+        y1 = (y+1) * grid_height - LINESPACE
 
     adj = mz.getAdjNode(x,y,dir)
     s1 = ",".join([str(x), str(y), str(dir)])
@@ -103,15 +136,14 @@ def drawWall(x, y, dir, unblocked):
         c.delete(s1)
         c.delete(s2)
     else:
-        c.create_line(x0,y0,x1,y1,fill="white", width = 2, tag = (s1,"wall"))
+        c.create_line(x0,y0,x1,y1,fill="white", width = 1, tag = (s1,"wall"))
 
 def randomize(event):
     # creates random maze
+
+    newMaze()
+
     timer = time()
-    mz.start = None
-    mz.end = None
-    c.delete("start")
-    c.delete("end")
 
     for y in range(maze_h):
         for x in range(maze_w):
@@ -128,10 +160,10 @@ def randomize(event):
 
 def drawGrid():
     # draw the grid (corner circles)
-
+    c.delete("grid")
     for x in range(maze_w + 1):
         for y in range(maze_h + 1 ): 
-            c.create_oval(grid_width*x-2, grid_height*y-2,grid_width*x+2, grid_height*y+2, fill="green", outline="green", tag = "grid")
+            c.create_oval(grid_width*x-GRID_RAD, grid_height*y-GRID_RAD,grid_width*x+GRID_RAD, grid_height*y+GRID_RAD, fill="green", outline="green", tag = "grid")
 
 def drawMaze():
     # refresh maze visuals
@@ -198,24 +230,40 @@ def solve(event):
 # GUI creation
 
 c = Canvas(master, width = canvas_w, height = canvas_h, bg="black", highlightthickness=0)
-c.grid(row=1, columnspan=5)
+c.grid(row=1, columnspan=10)
 #c.pack(expand=YES, fill=BOTH)
-l1 = Label(master,text="Maze size: " + str(maze_w) + " x " + str(maze_h),bg = DARKGRAY, fg="white")
-l1.grid(row=2)
+l1 = Label(master,text="Width: ",bg = DARKGRAY, fg="white")
+l1.grid(row=2, column=1, sticky = W)
+
+e_width = Entry(master, bg="black", fg="yellow", width = 3, justify="right", relief = FLAT)
+e_width.grid(row=2, column=2, sticky = W)
+e_width.insert(0, str(maze_w))
+
+l2 = Label(master,text="Height: ",bg = DARKGRAY, fg="white")
+l2.grid(row=2,column=3)
+
+e_height = Entry(master, bg="black", fg="yellow", width = 3, justify="right", relief = FLAT)
+e_height.grid(row=2, column=4)
+e_height.insert(0, str(maze_h))
+
+b_new = Button(master,text="New", fg="white", bg = GRAY,relief=FLAT, width = 10)
+b_new.grid(row=2, column=6, sticky=W+E)
+b_new.bind("<Button-1>", newMaze)
+
 b_rand = Button(master,text="Randomize", fg="white", bg = GRAY,relief=FLAT, width = 10)
-b_rand.grid(row=2, column=2)
+b_rand.grid(row=2, column=7, sticky=W+E)
 b_rand.bind("<Button-1>", randomize)
 
 b_solve = Button(master,text="Solve", fg="white", bg = GRAY,relief=FLAT, width = 10)
-b_solve.grid(row=2, column=3)
+b_solve.grid(row=2, column=8, sticky=W+E)
 b_solve.bind("<Button-1>", solve)
 
 b_exit = Button(master,text="Exit", fg="white", bg = GRAY,relief=FLAT, width = 10)
-b_exit.grid(row=2, column=4)
+b_exit.grid(row=2, column=9, sticky=W+E)
 b_exit.bind("<Button-1>", exit)
 
 statusbar = Label(master, text="", bd=1, bg = DARKGRAY, fg = "yellow", pady=5, padx=5)
-statusbar.grid(row=3, columnspan = 5, sticky=W+E)
+statusbar.grid(row=3, columnspan = 10, sticky=W+E)
 
 drawGrid()
 
